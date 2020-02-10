665be50c9dcb1488ff5dec19c102c08d38eb68d8
Ben Meredith <ben.meredith@gmail.com>
2020-02-10 07:01:08 -0500

    Create wpbr-deque-scripts.php

---
+++ wpbr/wpbr-deque-scripts.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ *  Dequeue The WPBR style on a single admin page. 
+ *  This snippet specifically removes it from the WPML String Translation admin pages, where it causes a conflict. 
+ *
+ **/
+ 
+function wpbr_dequeue_style() {
+	global $pagenow;
+	var_dump($_GET['page']);
+	if( in_array( $pagenow, array('admin.php') ) && ( $_GET['page'] == 'wpml-string-translation/menu/string-translation.php' ) ) {
+    	wp_dequeue_style( 'wpbr-admin-main-styles' );
+    }
+}
+add_action( 'admin_enqueue_scripts', 'wpbr_dequeue_style', 100 );
